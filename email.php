8b93778d770134454bb42af145b0e8a5c84f6f1b
simonfroggatt <simon@totalsafetygroup.co.uk>
2020-03-27 10:26:23 +0000

    email test for new server

--- email.php
+++ email.php
@@ -1,5 +1,19 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(-1);
-mail ('simonfroggatt76@gmail.com', 'Postfix Test', 'A test email') || print_r(error_get_last());
+$email_to = "simonfroggatt76@googlemail.com";
+$email_from = "test@crownsigns.co.uk";
+$message = $_POST["message"];
+$email_subject = "Test gmail from website";
+$headers = "From: " . $email_from . "\n";
+$headers .= "Reply-To: " . $email_from . "\n";
+
+$message = "This is a test email to check gmail accounts can receive from Fasthost server";
+
+ini_set("sendmail_from", $email_from);
+$sent = mail($email_to, $email_subject, $message, $headers, "-f" .$email_from);
+if ($sent)
+{
+    echo "Email sent";
+} else {
+    echo "There has been an error sending your comments. Please try later.";
+}
